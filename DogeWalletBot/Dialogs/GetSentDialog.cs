f7d0aaff684b6aa2e845049c70785ed3f8460838
Alexey_k <Alexey_k@fast-report.com>
2017-12-21 14:24:06 +0300

    added sent command

---
+++ DogeWalletBot/Dialogs/GetSentDialog.cs
@@ -0,0 +1,76 @@
+﻿using Budget.Bot.DAL;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+using System;
+using System.Threading.Tasks;
+
+namespace DogeWalletBot.Dialogs
+{
+    [Serializable]
+    internal class GetSentDialog : IDialog<object>
+    {
+        private int attempts = 3;
+        public string ExceptionMessage { get; set; } = "There was some errors, enter valid DogeCoin wallet address, please!";
+        private string ExceptionFinalMessage { get; set; } = $"Wow, we can't get sent DogeCoins. May be address is wrong, check it, please and try again later.";
+
+        public async Task StartAsync(IDialogContext context)
+        {
+            context.Wait(MessageReceivedAsync);
+            await Task.CompletedTask;
+        }
+
+        private async Task MessageReceivedAsync(IDialogContext context, IAwaitable<IMessageActivity> result)
+        {
+            var message = await result;
+            string address = "";
+            if ((message.Text != null) && (message.Text.Trim().Length > 0))
+            {
+                if (message.Text.Contains(" ")) //if "/sent [address]" command
+                {
+                    address = message.Text.Replace("/sent ", "").Trim();
+                }
+                else
+                    context.UserData.TryGetValue("wallet", out address);
+
+                if (!string.IsNullOrEmpty(address))
+                    try
+                    {
+                        var sent = await Client.GetSentAsync(address);
+                        if (sent.Success == 1)
+                        {
+                            await context.PostAsync($"Value of sent DogeCoin's from {address} address is: {sent.Sent}.");
+                            context.Done(0);
+                        }
+                        else
+                            await ProcessErrors(context);
+                    }
+                    catch (Exception ex)
+                    {
+                        await context.PostAsync(ex.Message);
+                        await ProcessErrors(context);
+                    }
+                else
+                {
+                    await context.PostAsync($"Set wallet addres first (/setwallet [addrss]) or call /sent [address] command!");
+                    context.Fail(new Exception("DogeCoin wallet address wasn't saved!"));
+                }
+            }
+        }
+
+        private async Task ProcessErrors(IDialogContext context)
+        {
+            --attempts;
+            if (attempts > 0)
+            {
+                await context.PostAsync(ExceptionMessage);
+                context.Wait(MessageReceivedAsync);
+            }
+            else
+            {
+                /* Fails the current dialog, removes it from the dialog stack, and returns the exception to the 
+                    parent/calling dialog. */
+                context.Fail(new TooManyAttemptsException(ExceptionFinalMessage));
+            }
+        }
+    }
+}
---
+++ DogeWalletBot/Model/SentEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DogeWalletBot.Model
+{
+    public class SentEntity
+    {
+        public string Sent { get; set; }
+        public int Success { get; set; }
+    }
+}
