ec2c0745b4a80466ad81478a2806604f17c340bb
Alexey_k <Alexey_k@fast-report.com>
2018-05-03 09:49:40 +0300

    added required classes

---
+++ DogeWalletBot/Model/ExchangeRate.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DogeWalletBot.Model
+{
+    public class ExchangeRate
+    {
+        public string Name { get; set; }
+        public string Price_usd { get; set; }
+        public string Price_btc { get; set; }
+    }
+}
---
+++ DogeWalletBot/Model/ReceivedTransaction.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DogeWalletBot.Model
+{
+    public class ReceivedTransaction
+    {
+        public string Txid { get; set; }
+        public string Value { get; set; }
+        public string Confirmations { get; set; }
+    }
+
+    public class ReceivedTransactionsEntity
+    {
+        public string Address { get; set; }
+        public List<ReceivedTransaction> Txs { get; set; }
+    }
+
+    public class ReceivedTransactionsResponse
+    {
+        public string Status { get; set; }
+        public ReceivedTransactionsEntity Data { get; set; }
+    }
+}
